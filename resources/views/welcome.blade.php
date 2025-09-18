30455432b50a2b517a29c43ee4428555d5d7f2c6
Marko Jezerčić <marko_jez@live.com>
2025-09-18 18:12:02 +0200

    Promjena naslova

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     
-        <h1>Welcome vjezba</h1>
+        <h1>Welcome back!</h1>
 
         <p>Hope you will enjoy time on this website today</p>
             
